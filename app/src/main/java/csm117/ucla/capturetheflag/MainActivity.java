5af5e174ffe11c22896f7423925824f79eda4898
Brian Kwak <briankwak49@gmail.com>
2017-05-16 22:19:05 -0700

    starting a repository!!!! woohoo

---
+++ app/src/main/java/csm117/ucla/capturetheflag/MainActivity.java
@@ -0,0 +1,13 @@
+package csm117.ucla.capturetheflag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
