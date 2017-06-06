ee5e2e2833a2a824cc898ce7973a9191cfd9a26f
Matt <matthewnuesca@gmail.com>
2017-06-06 00:39:05 -0700

    Cannot use return button on the main menu

--- app/src/main/java/csm117/ucla/capturetheflag/MainActivity.java
+++ app/src/main/java/csm117/ucla/capturetheflag/MainActivity.java
@@ -3,19 +3,15 @@ package csm117.ucla.capturetheflag;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -77,4 +73,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, JoinGameActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    public void onBackPressed(){}
 }
+
+
